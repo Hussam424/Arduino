d0ed2b6bb6ec1fc3d843e4cafd685e8e5a08a41e
hussa <hussamalhayek424@gmail.com>
2024-10-01 12:04:07 +0100

    add 2 fingers detected

--- GestureBasedLEDControl.cpp
+++ GestureBasedLEDControl.cpp
@@ -1,12 +1,16 @@
 const int led1 = 2;
 const int led2 = 3;
 const int led3 = 4;
+const int led4 = 5;
+const int led5 = 6;
 
 void setup() {
   Serial.begin(9600);  // Start serial communication
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
 }
 
 void loop() {
@@ -18,14 +22,34 @@ void loop() {
       digitalWrite(led1, HIGH);
       digitalWrite(led2, LOW);
       digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
     } else if (data == '2') {
       digitalWrite(led1, HIGH);
       digitalWrite(led2, HIGH);
       digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
     } else if (data == '3') {
       digitalWrite(led1, HIGH);
       digitalWrite(led2, HIGH);
       digitalWrite(led3, HIGH);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+    }
+    else if (data == '4') {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, LOW);
+    }
+    else if (data == '5') {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, HIGH);
     }
   }
 }
